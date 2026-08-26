4e489fc13f3aeb18d873469206247b3aece1ba83
AnasElgamed8 <anashossamhafez@gmail.com>
2026-08-26 17:39:52 +0300

    feat: Semi-finished streamlit integration

--- src/app.py
+++ src/app.py
@@ -4,7 +4,13 @@ from Sheet import Sheet
 
 test_sheet = Sheet()
 st.title("Arcaea random chart picker")
-min_constant = st.number_input("Pick the minimum allowed constant:",step=0.1,min_value=1.0,)
-max_constant = st.number_input("Pick the maximum allowed constant:",step=0.1,min_value=min_constant)
+
+left_column, right_column = st.columns(2)
+
+min_constant = left_column.number_input("Pick the minimum allowed constant:",step=0.1,min_value=1.0,max_value=12.0,value=1.0,key="mini") 
+
+max_constant = right_column.number_input("Pick the maximum allowed constant:",step=0.1,min_value=min_constant,max_value=12.0,value=12.0,key="maxi")
 size = st.number_input("How many charts do you want?",1)
-st.dataframe(test_sheet.true_random(min_constant,max_constant,size))
+
+if st.button("Randomize!",width="stretch"):
+    st.dataframe(test_sheet.true_random(min_constant,max_constant,size),hide_index=True)
