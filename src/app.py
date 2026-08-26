8aa6380b8e09796eb9c036e2b2f5f31cd387a329
AnasElgamed8 <anashossamhafez@gmail.com>
2026-08-26 17:45:34 +0300

    feat: Cache the data loading function

--- src/app.py
+++ src/app.py
@@ -2,7 +2,13 @@ import streamlit as st
 
 from Sheet import Sheet
 
-test_sheet = Sheet()
+
+@st.cache_resource
+def load_sheet():
+    return Sheet()
+
+test_sheet = load_sheet()
+
 st.title("Arcaea random chart picker")
 
 left_column, right_column = st.columns(2)
@@ -12,5 +18,5 @@ min_constant = left_column.number_input("Pick the minimum allowed constant:",ste
 max_constant = right_column.number_input("Pick the maximum allowed constant:",step=0.1,min_value=min_constant,max_value=12.0,value=12.0,key="maxi")
 size = st.number_input("How many charts do you want?",1)
 
-if st.button("Randomize!",width="stretch"):
+if st.button("Randomize!",width="stretch",type='primary'):
     st.dataframe(test_sheet.true_random(min_constant,max_constant,size),hide_index=True)
