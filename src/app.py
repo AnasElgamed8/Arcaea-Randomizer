128201ce14ac1ff004ac8db9a68274b92a8f50ed
AnasElgamed8 <anashossamhafez@gmail.com>
2026-08-26 23:12:51 +0300

    feat: Added some social media icons and an about section (TBC)

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 from Sheet import Sheet
+from st_social_media_links import SocialMediaIcons
 
 
 @st.cache_resource
@@ -8,8 +9,30 @@ def load_sheet():
     return Sheet()
 
 
+@st.cache_data
+def load_icons():
+    social_media_links = [
+        "https://www.youtube.com/@AnasElgamed",
+        "https://www.github.com/AnasElgamed8",
+        "https://www.reddit.com/user/Practical_Trouble165",
+        "https://www.discord.com/users/anaselgamed",
+    ]
+    SocialMediaIcons(social_media_links).render(sidebar=True)
+
+
 test_sheet = load_sheet()
 
+st.set_page_config(
+    page_title="Arcaea Randomizer By AnasElgamed8",
+    menu_items={
+        "About": """Hello! Anas here.
+        \nI wrote this app because I was bored (lol)
+        \nAnd it's a fun way to implement things I learn along the way.
+        \nHope that it improves your Arcaea experience even by a little!
+        \nIf you find any errors or have any suggestions. Feel free to open an issue on Github.
+        \nOr contact me via Discord [@anaselgamed](https://discordapp.com/users/anaselgamed)"""
+    },
+)
 st.title("Arcaea random chart picker")
 st.sidebar.title("Options")
 
@@ -48,3 +71,5 @@ if st.button("Randomize!", width="stretch", type="primary"):
     st.dataframe(
         test_sheet.true_random(min_constant, max_constant, size), hide_index=True
     )
+st.sidebar.divider()
+load_icons()
